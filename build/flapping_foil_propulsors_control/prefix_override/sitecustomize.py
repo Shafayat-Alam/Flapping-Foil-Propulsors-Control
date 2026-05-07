5ba75a3e7a7c0c3f13ab5ac3e81190b2acf7d1f5
Shafayat Alam <shafayatalam2004@gmail.com>
2026-05-07 19:28:21 -0400

    Clean repository: remove old files, keep only src/, .py, .sh, and README.md

--- build/flapping_foil_propulsors_control/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/shafa/flapping-propulsors-control/install/flapping_foil_propulsors_control'
